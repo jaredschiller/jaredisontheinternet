9c467f2bcdfcc28762e3f7680d8fdf756d663973
Jared Schiller <jaredpschiller@gmail.com>
2018-04-07 11:42:01 -0500

    adding a route

--- app.js
+++ app.js
@@ -3,8 +3,21 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
+app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+app.get('/', function(req, res){
+    res.render('test')
+
+})
+
+
+app.get('/todo_v1/', function(req, res){
+    res.send('welcome to todo v21 test');
+
+})
 
 app.listen(8080, () => console.log('Example app listening on port 8080!'));
